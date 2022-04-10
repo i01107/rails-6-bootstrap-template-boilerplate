5905ba46f292afe24e134ba8ae7ca207cfafd3b7
Fiqi Fitransyah <fiqi.i01107@gmail.com>
2022-04-10 11:44:21 +0700

    Implement basic stuffs : - jQuery, popper, Bootstrap - a template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,20 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import jQuery from "jquery"
+
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+// custom
+import "bootstrap"
+// import "../stylesheets/application.scss"
+// import "datatables.net-bs5"
+// import "datatables.net-buttons-bs5"
+import "./perfect-scrollbar.jquery.min"
+import "./waves"
+import "./sidebarmenu"
+import "./custom"
 
 Rails.start()
 Turbolinks.start()
